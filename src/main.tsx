61864c920e03fd124fbed92bf8312888eacf27a2
unknown <75647984+ha308ing@users.noreply.github.com>
2024-06-07 16:28:22 +0400

    hide pixel glass

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./components/app/app";
 import "./index.css";
-import "./pixel-glass";
+// import "./pixel-glass";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
